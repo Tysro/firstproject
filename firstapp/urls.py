aa1acad22bee54420f72b233b2b442f726cc7566
Tysro <sanoop.sreevalsam@gmail.com>
2022-02-03 09:39:58 +0530

    Added Bootstrap, learned how to create templates for the entire project

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from firstapp import views
 import firstapp
 
-app_name = 'firstapp'
+app_name = 'firstapp'  #this is a namespace the variable has special meaning on django with name of the app 
+# we can write more specific urls like name of the app plus urls.
 
 urlpatterns = [
     path('',views.index, name='index' ),
-    path('book/<int:book_id>/',views.detail, name='detail' ),
+    path('book<int:book_id>/',views.detail, name='detail' ),
 ]
